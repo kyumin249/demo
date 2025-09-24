e310c8abf19da540007f145163541b2c0d9cb514
kyumin <kyumin1429@naver.com>
2025-09-24 14:03:08 +0900

    testrepository

---
+++ src/main/java/com/example/demo/repository/TestRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository; // JPA 필수 등록
+import org.springframework.stereotype.Repository; // 빈 등록
+import com.example.demo.domain.TestDB; // 도메인 연동
+@Repository
+public interface TestRepository extends JpaRepository<TestDB, Long> {
+    // TestDB라는 도메인과 Long 타입의 PK를 관리하는 JPA 레포지토리
+    TestDB findByName(String name);
+}
